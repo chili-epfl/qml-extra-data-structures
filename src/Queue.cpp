0b581a6460d8cffc9febc92b0426da42f43d08c2
jgrondier <juliengrondier@gmail.com>
2018-04-19 15:41:27 +0200

    Added Queue data structure

---
+++ src/Queue.cpp
@@ -0,0 +1,6 @@
+#include "Queue.h"
+
+Queue::Queue()
+{
+
+}
---
+++ src/Queue.h
@@ -0,0 +1,17 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include <QQuickItem>
+
+class Queue : public QQuickItem
+{
+    Q_OBJECT
+public:
+    Queue();
+
+signals:
+
+public slots:
+};
+
+#endif // QUEUE_H
